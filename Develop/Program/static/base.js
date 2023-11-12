c9576b030e4495a42bbb843ee6e83a09c6e4e877
YojiUtsumi <cocosuke.pc.one@gmail.com>
2023-11-11 20:47:10 -0800

    fix about header

--- Develop/Program/static/base.js
+++ Develop/Program/static/base.js
@@ -3,7 +3,9 @@ window.addEventListener('resize', function () {
 
     if (width < 1250) {
         $('header>div').css('padding', '0 10px');
+        $('body>div').css('padding', '0 10px');
     } else {
+        $('body>div').css('padding', '');
         $('header>div').css('padding', '');
     }
   })
